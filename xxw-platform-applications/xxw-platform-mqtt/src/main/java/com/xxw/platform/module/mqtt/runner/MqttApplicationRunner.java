f9600d815c160a8166fdc2505bfae8ef36799542
liaoxiting <liaoxiting@jtexpress.com>
2023-09-12 15:15:50 +0800

    Adjusting the mqtt connection

--- xxw-platform-applications/xxw-platform-mqtt/src/main/java/com/xxw/platform/module/mqtt/runner/MqttApplicationRunner.java
+++ xxw-platform-applications/xxw-platform-mqtt/src/main/java/com/xxw/platform/module/mqtt/runner/MqttApplicationRunner.java
@@ -1,7 +1,10 @@
 package com.xxw.platform.module.mqtt.runner;
 
+import com.xxw.platform.module.common.constants.Constants;
 import com.xxw.platform.starter.mqtt.publish.IMqttPublish;
 import com.xxw.platform.starter.mqtt.service.MqttService;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
@@ -9,10 +12,11 @@ import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
 
+@Slf4j
 @Component
 public class MqttApplicationRunner implements ApplicationRunner {
 
-    @Value("${spring.mqtt.subscribe.clientId}")
+    @Value("${spring.mqtt.clientId}")
     private String clientId;
 
     @Resource
@@ -23,16 +27,16 @@ public class MqttApplicationRunner implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        mqttService.addTopic("ruleEngine", 0);
-        mqttService.addTopic("$queue/willTopic", 1);
-        mqttService.addTopic("$queue/onlineTopic", 1);
-        String payload = "[" + clientId + "] 已连接...";
-        mqttPublish.sendToMqtt("onlineTopic", 1, payload);
-        //共享订阅
-        mqttService.addTopic("$queue/sharedSubscription", 1);
-        //共享群组订阅
-        mqttService.addTopic("$share/222/sharedGroupSubscription", 1);
+        try {
+            String payload = "[" + clientId + "] 已连接...";
+            mqttPublish.sendToMqtt(Constants.CommonConstant.ONLINE_TOPIC, 1, payload);
 
-        mqttService.addTopic("test21", 1);
+//            //共享订阅
+//            mqttService.addTopic("$queue/sharedSubscription", 1);
+//            //共享群组订阅
+//            mqttService.addTopic("$share/222/sharedGroupSubscription", 1);
+        } catch (Exception e) {
+            log.error("mqtt 错误：{}", ExceptionUtils.getStackTrace(e));
+        }
     }
 }
